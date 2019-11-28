0e7178cee27e2544dcc387dc1b9e1155628dc873
Ray Oliveira <reinildes.jmo@gmail.com>
2019-11-28 01:31:06 -0300

    improve styles

--- src/App.js
+++ src/App.js
@@ -2,33 +2,50 @@ import React from 'react';
 import './App.css';
 import Task from './Task.js'
 
+const backend = 'http://127.0.0.1:5000'
+
 class App extends React.Component {
   
   state = {
-    todos : [{
-            desc: 'Buy the Cooffee',
-            checked: true,
-            id: 1
-          }]
+    todos : [],
+    user: null
   }
 
   render(){
     return (
+      
       <div className="App">
         <div className="App-header">
-          <main>
-            <input type="text" 
-              placeholder="Add a new Task" 
-              onKeyUp={(e) => this.addTask(e)}
-              className="New-task"/>
+          
+          {this.state.user == null
+          ?
+            <div>
+                  <input type="text" 
+                  placeholder="Please inform your name and hit enter" 
+                  onKeyUp={(e) => this.selectUser(e)}
+                  className="UserName"
+                  autoFocus={true}/>
+            </div>
+          : 
+          <div>
+            <p>{this.state.user}, Welcome to this Simple To-do list!</p>
+            <main>
+              
+              <input type="text" 
+                placeholder="Add a new Task" 
+                onKeyUp={(e) => this.addTask(e)}
+                className="New-task"
+                autoFocus={true}/>
 
-            {this.state.todos.map(todo =>
-              <Task todo={todo}
-                key={todo.id}
-                onCheck={this.onCheck}  
-                onDelete={this.onDelete}/>                
-            )}
-           </main>
+              {this.state.todos.map(todo =>
+                <Task todo={todo}
+                  key={todo.id}
+                  onCheck={this.onCheck}  
+                  onDelete={this.onDelete}/>                
+              )}
+            </main>
+          </div>
+          }
         </div>
       </div>
     );
@@ -46,13 +63,38 @@ class App extends React.Component {
             todos: [...this.state.todos, newTask]
           }
         )
+        
+        e.target.value = ''
+
+        fetch(backend + '/tasks/' + this.state.user, 
+          {method:'POST', body: JSON.stringify(newTask)})
+          .then(x => console.log(x.status))
     }
   }
 
-  onDelete = (e) =>{
+  selectUser = (e) =>{
+    if('Enter' === e.key){
+       this.setState(
+          {
+            user: e.target.value
+          }
+        )
+        fetch(backend + '/users', 
+          {method:'POST', body: JSON.stringify({user: e.target.value})})
+          .then(x => console.log(x.status))
+          
+        this.getTasks(e.target.value);
+    }
+  }
+
+  onDelete = (e) =>{  
     this.setState(
       {todos: [...this.state.todos.filter(todo => e.target.id != todo.id)]}
     )
+
+    fetch(backend + '/tasks/' + this.state.user + '/' + e.target.id , 
+          {method:'DELETE'})
+          .then(x => console.log(x.status))
   }
 
   onCheck = (e) =>{
@@ -66,6 +108,16 @@ class App extends React.Component {
     this.setState({
       todos: updatedTodos
     })
+
+    fetch(backend + '/tasks/' + this.state.user + '/' + e.target.id , 
+          {method:'PUT'})
+          .then(x => console.log(x.status))    
+  }
+
+  getTasks(user){
+      fetch(backend + "/tasks/" + user)
+      .then(data => data.json())
+      .then(data => this.setState({todos: data}))
   }
 }
 
